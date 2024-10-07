bf9ead94ca52dcc82edfa1e32f8834fb0b2a1d41
Sylvain Marroufin <marroufin.sylvain@gmail.com>
2024-10-07 22:56:59 +0200

    chore(release): setup release-it

---
+++ .release-it.ts
@@ -0,0 +1,30 @@
+import type { Config } from 'release-it'
+
+export default {
+  git: {
+    commitMessage: 'chore(release): v${version}',
+    requireBranch: "main",
+    tagName: 'v${version}',
+  },
+  github: {
+    release: true,
+    releaseName: 'v${version}',
+    web: true,
+  },
+  npm: {
+    publish: true,
+  },
+  hooks: {
+    'before:init': ['git pull', 'pnpm lint', 'pnpm docs:typecheck'],
+    'after:bump': 'npm run build',
+  },
+  plugins: {
+    '@release-it/conventional-changelog': {
+      preset: {
+        name: 'conventionalcommits',
+      },
+      infile: 'CHANGELOG.md',
+      header: '# Changelog',
+    },
+  },
+} satisfies Config
